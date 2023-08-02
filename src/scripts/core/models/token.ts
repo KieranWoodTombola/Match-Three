8aa57b6bb51b5209922ea84627a4c2bff0385368
kieranl.wood <kieran.wood@tombola.com>
2023-08-02 15:45:38 +0100

    Refactor Token
    -small changes to improve readability
    
    -removed simple get methods and set some properties to public
    
    -Removed unnecessary comments and obsolete features

--- src/scripts/core/models/token.ts
+++ src/scripts/core/models/token.ts
@@ -7,11 +7,8 @@ import { gsap } from 'gsap';
 export class Token extends Container {
 
     public matched: boolean;
-    private _parentID: number;
-    private parentSize: number;
-    public _verticalIndex: number;
-    private availWidth: number;
-    private availHeight: number;
+    public parentID: number;
+    public horizontalIndex;
     private skin: Spine;
     public skIndex: number = -1;
 
@@ -19,19 +16,16 @@ export class Token extends Container {
         return this._parentID;
     }
 
-    constructor (parentID: number, verticalIndex: number, size: number, availWidth: number, availHeight: number) {
+    constructor (parentID: number, horizontalIndex: number, size: number, skIndex: number, availWidth: number) {
         super();
 
         this.on('pointerdown', this.onClicked)
 
         this.matched = false;
         this.interactive = true;
-        this._parentID = parentID;
-        this.parentSize = size;
-        this._verticalIndex = verticalIndex;
-        this.availWidth = availWidth;
-        this.availHeight = availHeight;
-        this.skIndex = Math.round(Math.random() * (9 - 1) + 1);;
+        this.parentID = parentID;
+        this.horizontalIndex = horizontalIndex;
+        this.skIndex = skIndex;
         this.skin = new Spine(Assets.get('symbols').spineData);
         this.shuffleSkin();
         this.width = Math.ceil(this.skin.width)
@@ -50,7 +44,7 @@ export class Token extends Container {
     }
 
     public getLocation(): number[] {
-        const location = [this.parentID, this._verticalIndex];
+        const location = [this.parentID, this.horizontalIndex];
         return location;
     }
 
@@ -76,28 +70,6 @@ export class Token extends Container {
             alpha: 0.001,
             duration: 0.3
         });
-    }
-
-    public reveal(): void {
-        gsap.to(this, {
-            alpha: 1,
-            duration: 0.3
-        });
-    }
-
-    public moveTo(desiredArrayPosition: number): void {
-        const TargetLocation = (this.availHeight / (this.parentSize/desiredArrayPosition));
-        gsap.to(this, {
-            y: TargetLocation,
-            duration: 1
-        })
-    }
-
-    public testMoveTo(): void {
-        gsap.to(this, {
-        y: this.height,
-        duration: 1  
-        })
     }
 
 }
