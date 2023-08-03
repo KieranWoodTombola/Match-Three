94beac7155080145b8e2da5110a6421e4d76f8d4
kieranl.wood <kieran.wood@tombola.com>
2023-08-03 15:22:06 +0100

    Feature Column Tween to new positions
    -Started to develop tweens for tokens in new positions
        feature is currently limited.
        Currently the function examines a token, determines a
        destination and then tweens the examined token to that destination.
    
        To complete the feature, tokens may need to be added to a Container
        and then tweened

--- src/scripts/core/models/column.ts
+++ src/scripts/core/models/column.ts
@@ -7,22 +7,100 @@ export class Column extends Container{
     public tokens: Token[] = [];
     private columnID: number;
     private columnSize: number;
+    private availHeight: number;
 
     constructor(columnID: number, columnSize: number, availWidth: number, availHeight: number) {
         super()
 
         this.columnID = columnID;
         this.columnSize = columnSize;
+        this.availHeight = availHeight;
 
         for(var i = 0; i < this.columnSize; i++) {
             const randomNumber = Math.round(Math.random() * (9 - 1) + 1);
 
-            const newToken = new Token(this.columnID, i, this.columnSize, randomNumber, availWidth)
-            newToken.y = (availHeight / (columnSize/i));
+            const newToken = new Token(this.columnID, i, this.columnSize, randomNumber, availWidth, availHeight)
+            newToken.y = (availHeight / (this.columnSize/i));
 
             this.tokens.push(newToken);
             this.addChild(newToken);
         }
+    }
+
+    public processMatches(): void {
+            const matchedTokens: Token [] = [];
+            const unmatchedTokens: Token [] = [];
+
+            //this.hideMatches();
+            
+            //build an array of matched tokens and an array of unmatched tokens
+            for(let i = 0; i < this.tokens.length; i++) {
+                const target = this.tokens[i]
+                if(target.matched) {
+                    target.shuffleSkin();
+                    target.hide();
+                    matchedTokens.push(target);
+                }
+
+                if(!target.matched) {
+                    unmatchedTokens.push(target);
+                }
+            }
+
+            //the unmatched tokens on the screen
+            //find coordinate of the place to move to
+            for(let columnInspector: number = 0; columnInspector < this.tokens.length-1; columnInspector++) {
+
+                if(//columnInspector+1 <= this.tokens.length-1 
+                     !this.tokens[columnInspector].matched 
+                    ) {
+                        console.log("columnInspector: ", columnInspector);
+                        let combo = columnInspector+1;
+                        for(let combo = columnInspector+1; combo <= this.tokens.length-1; combo++) {
+                            if(!this.tokens[combo].matched) { 
+                                break; 
+                            }
+                            else 
+                                console.log("combo: ", combo);
+                                this.tokens[columnInspector].moveTo(combo);
+                        }
+                }
+            }
+            //tween there
+
+            this.tokens.forEach(token => {
+                token.matched = false;
+            })
+
+            // this.tokens = [...matchedTokens, ...unmatchedTokens];
+            // console.log(this.tokens);
+            // for(var i = 0; i < this.columnSize; i++) {
+            //     this.tokens[i].y = (this.availHeight / (this.columnSize/i));
+            //     this.tokens[i]._verticalIndex = i;
+            // }
+
+            //this.revealMatches();
+    }
+
+    public hideMatches(): void {
+        this.tokens.forEach(token => {
+            if(token.matched) {
+                token.hide()
+            }
+        });
+    }
+
+    public revealMatches(): void {
+        this.tokens.forEach(token => {
+            if(token.matched) {
+                token.reveal()
+                token.matched = false;
+            }
+        });
+    }
+
+    public getToken(Y: number) {
+        return this.tokens[Y];
     }
 
     public removeAllTokens(): void {
@@ -35,7 +113,4 @@ export class Column extends Container{
         }
     }
 
-    public getToken(Y: number) {
-        return this.tokens[Y];
-    }
 }
