a77157d983f64028162980586776742de52f26a1
kieranl.wood <kieran.wood@tombola.com>
2023-08-03 09:13:36 +0100

    Refactor Column
    - Small type changes and comment removal

--- src/scripts/core/models/column.ts
+++ src/scripts/core/models/column.ts
@@ -3,10 +3,10 @@ import { Token } from "./token";
 
 export class Column extends Container{
 
-    public tokens: Token[];
+    public tokens: Token[] = [];
     private columnID: number;
     private columnSize: number;
-    private availHeight: number;
+
 
     constructor(columnID: number, columnSize: number, availWidth: number, availHeight: number) {
         super();
@@ -24,73 +24,8 @@ export class Column extends Container{
         }
     }
 
-    public processMatches(): void {
-            //move tokens from original positions in to the furthest matched token position
-            this.columnTweens();
 
-            //rebuild and reset the column's tokens
-            this.rebuildTokens();
 
-    }
-
-    private columnTweens(): void {
-        const nonMatchCombo: Token [] = [];
-        for(let columnInspector: number = 0; columnInspector < this.tokens.length-1; columnInspector++) {
-            const target = this.tokens[columnInspector];
-            if(!target.matched) {
-                nonMatchCombo.unshift(target);
-                for(let combo = columnInspector+1; combo <= this.tokens.length-1; combo++) {
-                    if(!this.tokens[combo].matched){ break; }
-                    for(let i = 0; i < nonMatchCombo.length; i++){
-                        nonMatchCombo[i].moveTo(combo-i);
-                    }
-                }
-            }
-        }
-    }
-
-    private rebuildTokens(): void {
-        const matchedTokens: Token [] = [];
-        const unmatchedTokens: Token [] = [];
-        for(let i = 0; i < this.tokens.length; i++) {
-            const target = this.tokens[i]
-            if(target.matched) {
-                target.hide();
-                target.interactive = false;
-                matchedTokens.push(target);
-            }
-
-            else {
-                unmatchedTokens.push(target);
-            }
-        }
-        const newTokens = [...matchedTokens, ...unmatchedTokens]
-        for(var i = 0; i < this.columnSize; i++) {
-            newTokens[i]._verticalIndex = i;
-            newTokens[i].matched = false;
-        }
-        this.tokens = newTokens;
-    }
-
-    public hideMatches(): void {
-        this.tokens.forEach(token => {
-            if(token.matched) {
-                token.hide()
-            }
-        });
-    }
-
-    public revealMatches(): void {
-        this.tokens.forEach(token => {
-            if(token.matched) {
-                token.reveal()
-            }
-        });
-    }
-
-    public getToken(Y: number) {
-        return this.tokens[Y];
-    }
 
     public removeAllTokens(): void {
         this.tokens = [];
