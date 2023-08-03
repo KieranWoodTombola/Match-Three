9577d715144421177129e469907271bb5e1e51ff
kieranl.wood <kieran.wood@tombola.com>
2023-08-03 09:14:53 +0100

    Refactor Column
    - Small type changes and comment removal

--- src/scripts/core/models/column.ts
+++ src/scripts/core/models/column.ts
@@ -8,20 +8,15 @@ export class Column extends Container{
     private columnID: number;
     private columnSize: number;
 
-
     constructor(columnID: number, columnSize: number, availWidth: number, availHeight: number) {
         super()
 
         this.columnID = columnID;
         this.columnSize = columnSize;
-        // this.viewWidth = viewWidth;
-        // this.viewHeight = viewHeight;
 
         for(var i = 0; i < this.columnSize; i++) {
             const randomNumber = Math.round(Math.random() * (9 - 1) + 1);
 
-            //const newToken = new Token(ran, this.columnID, availWidth, columnSize,);
-            //parentID: number, locationIndex: number, size: number, skIndex: number, availWidth: number
             const newToken = new Token(this.columnID, i, this.columnSize, randomNumber, availWidth)
             newToken.y = (availHeight / (columnSize/i));
 
@@ -29,9 +24,6 @@ export class Column extends Container{
             this.addChild(newToken);
         }
     }
-
-
-
 
     public removeAllTokens(): void {
         this.tokens = [];
