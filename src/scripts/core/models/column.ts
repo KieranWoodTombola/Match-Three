acd6a9463a45c94ea0c8740ac87b2389dbc8c7db
kieranl.wood <kieran.wood@tombola.com>
2023-08-03 15:22:06 +0100

    Feature Column Tween to new positions
    -Started to develop tweens for tokens in new positions
        feature is currently limited.
        Currently the function examines a token, determines a
        destination and then tweens the examined token to that destination.
    
        To complete the feature, tokens may need to be added to a Container
        and then tweened

--- src/scripts/core/models/column.ts
+++ src/scripts/core/models/column.ts
@@ -14,33 +14,160 @@ export class Column extends Container{
         this.columnID = columnID;
         this.columnSize = columnSize;
         this.availHeight = availHeight;
+
         for(var i = 0; i < this.columnSize; i++) {
-            const newToken = new Token(this.columnID, i, this.columnSize, availWidth, availHeight);
+            const randomNumber = Math.round(Math.random() * (9 - 1) + 1);
+
+            const newToken = new Token(this.columnID, i, this.columnSize, randomNumber, availWidth, availHeight)
             newToken.y = (availHeight / (this.columnSize/i));
+
             this.tokens.push(newToken);
             this.addChild(newToken);
         }
     }
 
-    public removeAllTokens(): void {
-        this.tokens = [];
-    }
+    public processMatches(): void {
+            const matchedTokens: Token [] = [];
+            const unmatchedTokens: Token [] = [];
 
-    private columnTweens(): void {
-        const nonMatchCombo: Token [] = [];
-        for(let columnInspector: number = 0; columnInspector < this.tokens.length-1; columnInspector++) {
-            const target = this.tokens[columnInspector];
-            if(!target.matched) {
-                nonMatchCombo.unshift(target);
-                for(let combo = columnInspector+1; combo <= this.tokens.length-1; combo++) {
-                    if(!this.tokens[combo].matched) { 
-                        break;
-                    }
-                    for(let i = 0; i < nonMatchCombo.length; i++){
-                        nonMatchCombo[i].moveTo(combo-i);
-                    }
+            //this.hideMatches();
+            
+            //build an array of matched tokens and an array of unmatched tokens
+            for(let i = 0; i < this.tokens.length; i++) {
+                const target = this.tokens[i]
+                if(target.matched) {
+                    target.shuffleSkin();
+                    target.hide();
+                    matchedTokens.push(target);
+                }
+
+                if(!target.matched) {
+                    unmatchedTokens.push(target);
                 }
             }
+
+            //the unmatched tokens on the screen
+            //find coordinate of the place to move to
+            for(let columnInspector: number = 0; columnInspector < this.tokens.length-1; columnInspector++) {
+
+                if(//columnInspector+1 <= this.tokens.length-1 
+                     !this.tokens[columnInspector].matched 
+                    ) {
+                        console.log("columnInspector: ", columnInspector);
+                        let combo = columnInspector+1;
+                        for(let combo = columnInspector+1; combo <= this.tokens.length-1; combo++) {
+                            if(!this.tokens[combo].matched) { 
+                                break; 
+                            }
+                            else 
+                                console.log("combo: ", combo);
+                                this.tokens[columnInspector].moveTo(combo);
+                        }
+                }
+            }
+            //tween there
+
+            this.tokens.forEach(token => {
+                token.matched = false;
+            })
+
+            // this.tokens = [...matchedTokens, ...unmatchedTokens];
+            // console.log(this.tokens);
+            // for(var i = 0; i < this.columnSize; i++) {
+            //     this.tokens[i].y = (this.availHeight / (this.columnSize/i));
+            //     this.tokens[i]._verticalIndex = i;
+            // }
+
+            //this.revealMatches();
+    }
+
+    public hideMatches(): void {
+        this.tokens.forEach(token => {
+            if(token.matched) {
+                token.hide()
+            }
+        });
+    }
+
+    public revealMatches(): void {
+        this.tokens.forEach(token => {
+            if(token.matched) {
+                token.reveal()
+                token.matched = false;
+            }
+        });
+    }
+
+    public getToken(Y: number) {
+        return this.tokens[Y];
+    }
+
+    public processMatches(): void {
+            const matchedTokens: Token [] = [];
+            const unmatchedTokens: Token [] = [];
+
+            //this.hideMatches();
+            
+            //build an array of matched tokens and an array of unmatched tokens
+            for(let i = 0; i < this.tokens.length; i++) {
+                const target = this.tokens[i]
+                if(target.matched) {
+                    target.shuffleSkin();
+                    target.hide();
+                    matchedTokens.push(target);
+                }
+
+                if(!target.matched) {
+                    unmatchedTokens.push(target);
+                }
+            }
+
+            //the unmatched tokens on the screen
+            //find coordinate of the place to move to
+            for(let columnInspector: number = 0; columnInspector < this.tokens.length-1; columnInspector++) {
+
+                if(//columnInspector+1 <= this.tokens.length-1 
+                     !this.tokens[columnInspector].matched 
+                    ) {
+                        console.log("columnInspector: ", columnInspector);
+                        let combo = columnInspector+1;
+                        for(let combo = columnInspector+1; combo <= this.tokens.length-1; combo++) {
+                            if(!this.tokens[combo].matched) { 
+                                break; 
+                            }
+                            else 
+                                console.log("combo: ", combo);
+                                this.tokens[columnInspector].moveTo(combo);
+                        }
+                }
+            }
+            //tween there
+
+            this.tokens.forEach(token => {
+                token.matched = false;
+            })
+
+            // this.tokens = [...matchedTokens, ...unmatchedTokens];
+            // console.log(this.tokens);
+            // for(var i = 0; i < this.columnSize; i++) {
+            //     this.tokens[i].y = (this.availHeight / (this.columnSize/i));
+            //     this.tokens[i]._verticalIndex = i;
+            // }
+
+            //this.revealMatches();
+    }
+
+    public hideMatches(): void {
+        this.tokens.forEach(token => {
+            if(token.matched) {
+                token.hide()
+            }
+        });
+    }
+
+    public replaceAllTokens(newTokens: Token[]): void {
+        if(newTokens.length != this.tokens.length) {
+            return;
         }
     }
 
