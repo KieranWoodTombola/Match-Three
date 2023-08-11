610ac95aa75561b86440e1e798b6dcff4fc25cd9
kieranl.wood <kieran.wood@tombola.com>
2023-08-11 10:55:50 +0100

    Feature Matching Tokens Display on ScoreCard
    - Tokens matching on the same axis are displayed
    on the Scorecard to clearly display successful matches
    
    Tokens are seperated on finding a new skin for matched Tokens
    can encounter bugs where multiple combos of tokens with the
    same skin results in tokens displaying on top of each other

--- src/scripts/core/models/score-display.ts
+++ src/scripts/core/models/score-display.ts
@@ -1,14 +1,21 @@
 import { gsap } from "gsap";
 import { Container, Graphics, Text as PixiText } from "pixi.js";
+import { Token } from "./token";
 import { eventEmitter } from "../../../event-emitter";
 
 export class ScoreDisplay extends Container {
 
     private score: number = 0;
     private scoreText: PixiText = new PixiText(`${this.score}`);
+    private textContainer: Container = new Container();
+    private tokensContainer: Container = new Container();
+    private trackedSkin = -1;
+    private trackedHeight = -1;
 
     constructor() {
         super() 
+
+        eventEmitter.on('onMatch', this.recordMatchedTokens, this)
 
         const title: PixiText = new PixiText("SCORE");
         this.scoreText.style = {
@@ -16,19 +23,26 @@ export class ScoreDisplay extends Container {
         };
         const scoreCard = new Graphics();
         scoreCard.beginFill(0xFFFFFF);
-        scoreCard.drawRoundedRect(0, 0, title.width, title.height, 5);
+        scoreCard.drawRoundedRect(0, 0, title.width, title.height, 3);
         scoreCard.endFill();
         scoreCard.addChild(title);
         this.scoreText.position = {
             x: scoreCard.width * 0.5 - this.scoreText.width,
             y: scoreCard.height * 1.5
-        }
-        this.addChild(scoreCard);
-        this.addChild(this.scoreText);
-        console.log(this.score)
+        };
+        this.textContainer.addChild(scoreCard);
+        this.textContainer.addChild(this.scoreText);
+        this.addChild(this.textContainer);
         this.updateScore(50);
-    }
 
+        this.addChild(this.tokensContainer);
+
+        const marker = new Graphics();
+        marker.beginFill("red");
+        marker.drawCircle(this.textContainer.width/2, this.textContainer.height/2, 3);
+        marker.endFill();
+        this.textContainer.addChild(marker);
+    }
 
     private updateScore(targetScore: number) {
         gsap.to(this, {
@@ -37,9 +51,41 @@ export class ScoreDisplay extends Container {
             onUpdate: this.showScore.bind(this),
         })
     }
-    
+
     private showScore(): void {
         if(!this.scoreText) { console.log("crash"); return; }
         this.scoreText.text = Math.round(this.score);
     }
+
+    private recordMatchedTokens(tokens: Token[]): void {
+        const displayTokenContainer = new Container();
+        
+        tokens.forEach(token => {
+            console.log( this.trackedSkin, this.trackedHeight );
+            if(token.skIndex !== this.trackedSkin) {
+                this.trackedSkin = token.skIndex;
+                this.trackedHeight++ ;
+            }
+            const newToken = new Token (token.availWidth, token.availHeight, token.skIndex);
+            newToken.scale.x = 0.3;
+            newToken.scale.y = 0.3;
+            newToken.width = Math.floor(newToken.width);
+            newToken.x = Math.floor(((token.width* 0.75) * tokens.indexOf(token)) + Math.floor(token.width * 0.4));
+            newToken.y = (token.height/2 * this.trackedHeight) + (token.height * 0.4);
+            displayTokenContainer.addChild(newToken);
+        })
+        this.addChild(displayTokenContainer);
+        //displayTokenContainer.pivot.set(displayTokenContainer.width*0.5, displayTokenContainer.y * 0.5);
+        displayTokenContainer.position = {
+            x: (this.textContainer.width * 0.5) - (displayTokenContainer.width * 0.5), 
+            y: this.textContainer.height
+        };
+
+        // const square = new Graphics();
+        // square.beginFill("white");
+        // square.drawRect(0, 0, displayTokenContainer.width, displayTokenContainer.height);
+        // square.endFill();
+        // displayTokenContainer.addChild(square);
+    }
+
 }
