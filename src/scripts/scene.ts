0353e052892e08f61da96c96a782995086c266c1
kieranl.wood <kieran.wood@tombola.com>
2023-08-14 11:51:10 +0100

    Whoops - Fixed Accidental Merge

--- src/scripts/scene.ts
+++ src/scripts/scene.ts
@@ -1,6 +1,5 @@
 import { Assets, Container } from 'pixi.js'
 import { Grid } from './core/models/grid';
-import { ScoreDisplay } from './core/models/score-display';
 
 export class Scene extends Container {
     private viewWidth: number;
@@ -39,14 +38,7 @@ export class Scene extends Container {
         const grid = new Grid(6, this.gridWidth);
         this.addChild(grid);
 
-        const scoreDisplay = new ScoreDisplay();
         const remainingWidth = this.viewWidth - grid.width;
-        scoreDisplay.position = {
-            x: this.gridWidth + remainingWidth * 0.5,
-            y: this.viewHeight * 0.3
-        }
-
-        this.addChild(scoreDisplay);
     }
 
     public update(delta: number): void {
