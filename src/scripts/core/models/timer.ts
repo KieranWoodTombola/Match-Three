49b9cd58bd57233f4b1ceb7cb71389fc022bef15
Kieran W <kieran.wood@tombola.com>
2023-08-18 10:44:33 +0100

    Tiny Cleanup

--- src/scripts/core/models/timer.ts
+++ src/scripts/core/models/timer.ts
@@ -2,7 +2,7 @@ import { Container, Text as PixiText } from "pixi.js";
 import { gsap } from "gsap";
 import { eventEmitter } from "../../../event-emitter";
 
-export class Timer extends Container{
+export class Timer extends Container {
 
     private _totalTime: number;
     private _currentTime: number;
@@ -27,19 +27,18 @@ export class Timer extends Container{
     private formatTime(): void {
         let minutes = "0";
         let seconds = "0";
-        this._currentTime >= 60 ? minutes = Math.floor(this._currentTime/60).toString() : minutes = '0';
-        this._currentTime % 60 === 0 ? seconds = "0" :
-        seconds = (this._currentTime % 60).toString().padStart(2, "0");
-        this._timeText.text =  minutes + ":" + seconds;
+        this._currentTime >= 60 ? minutes = Math.floor(this._currentTime / 60).toString() : minutes = '0';
+        this._currentTime % 60 === 0 ? seconds = "0" : seconds = (this._currentTime % 60).toString().padStart(2, "0");
+        this._timeText.text = minutes + ":" + seconds;
     }
 
     private countdown() {
         gsap.to(this, {
             duration: 1,
-            _currentTime: this._currentTime -1,
-            onComplete: () =>{
+            _currentTime: this._currentTime - 1,
+            onComplete: () => {
                 this.formatTime()
-                if(this._currentTime > 0) { this.countdown() }
+                if (this._currentTime > 0) { this.countdown() }
                 else {
                     eventEmitter.emit('countdown');
                 }
@@ -48,4 +47,3 @@ export class Timer extends Container{
     }
 
 }
-    
